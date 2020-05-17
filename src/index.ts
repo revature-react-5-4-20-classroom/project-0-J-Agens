3f0237fff486e74dde41acac2ea5c43a20d9d540
J-Agens <johntagens@gmail.com>
2020-05-17 15:35:59 -0400

    added session handling, and database access configuration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
 import express from 'express';
 import { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import { sessionMiddleware } from './middleware/sessionMiddleware';
+import { connectionPool } from './repository';
+import { PoolClient, QueryResult } from 'pg';
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+
+app.use(sessionMiddleware);
+
+// ENDPOINTS
+
+app.post('/login', (req: Request, res: Response) => {
+
+});
+
+app.get('/users', (req: Request, res: Response) => {
+
+})
+
+// OPEN PORT
+app.listen(3000, () => {
+    console.log('app has started');
+    
+    // TESTING DB CONNECTION
+    connectionPool.connect().then((client: PoolClient) => {
+        console.log('connected to database');
+        client.query('SELECT * FROM reimbursements;').then((result: QueryResult) => {
+            console.log(result.rows);
+        }).catch((err) => {
+            console.log(err.message);
+            
+        })
+    })
+});
---
+++ src/routers/userRouter.ts
@@ -0,0 +1,3 @@
+import express, { Router, Request, Response } from 'express';
+import User from '../models/User';
+
